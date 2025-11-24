b2383dae56c0e9cb3a2c353de3176cc0cb912ff8
Diogo Guedes <diogo.f.guedes@tentwentyone.io>
2025-11-24 15:27:25 +0000

    fix: trivy and docker app references

--- infra/variables.tf
+++ infra/variables.tf
@@ -10,25 +10,6 @@ variable "resource_group_name" {
   default     = "rg-java-cicd"
 }
 
-resource "azurerm_kubernetes_cluster" "this" {
-  name                = "java-cicd-aks"
-  location            = azurerm_resource_group.this.location
-  resource_group_name = azurerm_resource_group.this.name
-  dns_prefix          = "java-cicd-aks"
 
-  default_node_pool {
-    name       = "default"
-    node_count = 1
-    vm_size    = "Standard_D2_v2"
-  }
-
-  identity {
-    type = "SystemAssigned"
-  }
-
-  tags = {
-    Environment = "Production"
-  }
-}
 
 
