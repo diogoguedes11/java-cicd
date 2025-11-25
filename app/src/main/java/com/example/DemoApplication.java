3b800839cf589554b71b84eda902467598c1963b
Diogo Guedes <diogo.f.guedes@tentwentyone.io>
2025-11-25 16:34:37 +0000

    feat: change app port and logger

--- app/src/main/java/com/example/DemoApplication.java
+++ app/src/main/java/com/example/DemoApplication.java
@@ -1,4 +1,7 @@
 package com.example;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.boot.SpringApplication;
@@ -6,14 +9,43 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
-    }
+  private static final java.util.logging.Logger logger = LoggerFactory.getLogger(DemoApplication.class);
+
+  public static void main(String[] args) {
+    logger.info("Application starting");
+    SpringApplication.run(DemoApplication.class, args);
+  }
 }
+
 @RestController
 class HelloController {
-    @GetMapping("/")
-    public String hello() {
-        return "Hello from Java CICD Demo!";
-    }
+  private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
+
+  @GetMapping("/")
+  public String hello() {
+    logger.info("Hello endpoint accessed");
+    return "Hello from Java CICD Demo!";
+  }
+}
+
+@RestController
+class HealthController {
+  private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
+
+  @GetMapping("/health")
+  public String health() {
+    logger.info("Health endpoint accessed");
+    return "OK";
+  }
+}
+
+@RestController
+class InfoController {
+  private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
+
+  @GetMapping("/info")
+  public String info() {
+    logger.info("Info endpoint accessed");
+    return "Java CICD Demo Application v1.0";
+  }
 }
