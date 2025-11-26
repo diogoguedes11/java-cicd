12819fb941075b9538352ad7d4a4a9de2b537ff8
Diogo Guedes <diogo.f.guedes@tentwentyone.io>
2025-11-26 17:37:35 +0000

    feat: add kubernetes

--- infra/provider.tf
+++ infra/provider.tf
@@ -8,6 +8,10 @@ terraform {
       source  = "hashicorp/helm"
       version = "=3.1.0"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "=2.38.0"
+    }
   }
   backend "azurerm" {}
 }
@@ -23,4 +27,10 @@ provider "helm" {
     client_key             = base64decode(azurerm_kubernetes_cluster.this.kube_config[0].client_key)
     cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.this.kube_config[0].cluster_ca_certificate)
   }
+}
+provider "kubernetes" {
+  host                   = azurerm_kubernetes_cluster.this.kube_config[0].host
+  client_certificate     = base64decode(azurerm_kubernetes_cluster.this.kube_config[0].client_certificate)
+  client_key             = base64decode(azurerm_kubernetes_cluster.this.kube_config[0].client_key)
+  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.this.kube_config[0].cluster_ca_certificate)
 }
