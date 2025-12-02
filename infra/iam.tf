bb5003add56c00efa0ef5b08ced60e07242b05a3
Diogo Guedes <diogo.f.guedes@tentwentyone.io>
2025-12-02 17:28:16 +0000

    feat: remove sm, change action to deploy to acr and add permissions for aks pull from the acr

--- infra/iam.tf
+++ infra/iam.tf
@@ -1,28 +1,9 @@
-# # Service principal for Github actions
-# resource "azuread_application" "github_actions" {
-#   display_name = "sp-github-actions-acr-push"
-# }
+resource "azurerm_role_assignment" "aks_to_acr" {
+  principal_id = azurerm_kubernetes_cluster.this.kubelet_identity[0].object_id
 
-# resource "azuread_service_principal" "github_actions" {
-#   client_id = azuread_application.github_actions.client_id
-# }
+  role_definition_name = "AcrPull"
 
-# resource "azuread_service_principal_password" "github_actions" {
-#   service_principal_id = azuread_service_principal.github_actions.id
-# }
+  scope = azurerm_container_registry.acr.id
 
-# # Assign "AcrPush" role to this Service Principal
-# resource "azurerm_role_assignment" "github_to_acr" {
-#   principal_id         = azuread_service_principal.github_actions.object_id
-#   role_definition_name = "AcrPush" # Permission to push images
-#   scope                = azurerm_container_registry.acr.id
-# }
-
-# output "github_actions_client_id" {
-#   value = azuread_application.github_actions.client_id
-# }
-
-# output "github_actions_client_secret" {
-#   value     = azuread_service_principal_password.github_actions.value
-#   sensitive = true
-# }
+  skip_service_principal_aad_check = true
+}
