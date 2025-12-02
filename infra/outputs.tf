59245b4528cad06b2d122b2af5f3216f108b68cc
Diogo Guedes <diogo.f.guedes@tentwentyone.io>
2025-12-02 16:26:36 +0000

    feat: add outputs to the outputs.tf

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -8,3 +8,16 @@ output "kube_config" {
 
   sensitive = true
 }
+output "acr_login_server" {
+  value = azurerm_container_registry.acr.login_server
+}
+
+output "acr_admin_username" {
+  value     = azurerm_container_registry.acr.admin_username
+  sensitive = true
+}
+
+output "acr_admin_password" {
+  value     = azurerm_container_registry.acr.admin_password
+  sensitive = true
+}
