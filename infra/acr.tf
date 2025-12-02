09f184da321af0b9f06732ea91aed42ec381d3b4
Diogo Guedes <diogo.f.guedes@tentwentyone.io>
2025-12-02 16:06:50 +0000

    feat: format acr and disable service principals for now

--- infra/acr.tf
+++ infra/acr.tf
@@ -1,9 +1,9 @@
 resource "azurerm_container_registry" "acr" {
-  name                     = "acr-javacicd${random_string.suffix.result}"
-  resource_group_name      = "${azurerm_resource_group.this.name}"
-  location                 = "${azurerm_resource_group.this.location}"
-  sku                      =  "Basic"
-  admin_enabled            = false
+  name                = "acr-javacicd${random_string.suffix.result}"
+  resource_group_name = azurerm_resource_group.this.name
+  location            = azurerm_resource_group.this.location
+  sku                 = "Basic"
+  admin_enabled       = true
 
   tags = {
     Environment = var.environment
@@ -14,12 +14,12 @@ output "acr_login_server" {
   value = azurerm_container_registry.acr.login_server
 }
 
-# output "acr_admin_username" {
-#   value = azurerm_container_registry.acr.admin_username
-#   sensitive = true
-# }
+output "acr_admin_username" {
+  value     = azurerm_container_registry.acr.admin_username
+  sensitive = true
+}
 
-# output "acr_admin_password" {
-#   value = azurerm_container_registry.acr.admin_password
-#   sensitive = true
-# }
+output "acr_admin_password" {
+  value     = azurerm_container_registry.acr.admin_password
+  sensitive = true
+}
--- infra/iam.tf
+++ infra/iam.tf
@@ -1,28 +1,28 @@
-# Service principal for Github actions
-resource "azuread_application" "github_actions" {
-  display_name = "sp-github-actions-acr-push"
-}
+# # Service principal for Github actions
+# resource "azuread_application" "github_actions" {
+#   display_name = "sp-github-actions-acr-push"
+# }
 
-resource "azuread_service_principal" "github_actions" {
-  client_id = azuread_application.github_actions.client_id
-}
+# resource "azuread_service_principal" "github_actions" {
+#   client_id = azuread_application.github_actions.client_id
+# }
 
-resource "azuread_service_principal_password" "github_actions" {
-  service_principal_id = azuread_service_principal.github_actions.id
-}
+# resource "azuread_service_principal_password" "github_actions" {
+#   service_principal_id = azuread_service_principal.github_actions.id
+# }
 
-# Assign "AcrPush" role to this Service Principal
-resource "azurerm_role_assignment" "github_to_acr" {
-  principal_id         = azuread_service_principal.github_actions.object_id
-  role_definition_name = "AcrPush" # Permission to push images
-  scope                = azurerm_container_registry.acr.id
-}
+# # Assign "AcrPush" role to this Service Principal
+# resource "azurerm_role_assignment" "github_to_acr" {
+#   principal_id         = azuread_service_principal.github_actions.object_id
+#   role_definition_name = "AcrPush" # Permission to push images
+#   scope                = azurerm_container_registry.acr.id
+# }
 
-output "github_actions_client_id" {
-  value = azuread_application.github_actions.client_id
-}
+# output "github_actions_client_id" {
+#   value = azuread_application.github_actions.client_id
+# }
 
-output "github_actions_client_secret" {
-  value     = azuread_service_principal_password.github_actions.value
-  sensitive = true
-}
+# output "github_actions_client_secret" {
+#   value     = azuread_service_principal_password.github_actions.value
+#   sensitive = true
+# }
