1433683afadc208108be823942e04eb5a665df3e
Diogo Guedes <diogo.f.guedes@tentwentyone.io>
2025-12-05 09:19:34 +0000

    feat: new changes to gitigfnore and increwase machine tier aks

--- infra/aks.tf
+++ infra/aks.tf
@@ -9,7 +9,7 @@ resource "azurerm_kubernetes_cluster" "this" {
   default_node_pool {
     name       = "default"
     node_count = 2
-    vm_size    = "Standard_B4ms"  # 4 vCPUs, 16 GB RAM
+    vm_size    = "Standard_B4ms" # 4 vCPUs, 16 GB RAM
   }
 
   identity {
